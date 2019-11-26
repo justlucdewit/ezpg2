e47dd6a98b8e131224a8824272cf5483234aaf00
Luc de wit <luc.dewit@hotmail.nl>
2019-11-26 22:21:55 +0100

    end of day

--- sketch.py
+++ sketch.py
@@ -1,14 +1,9 @@
 from ezpg2 import *
 
 def setup():
-    createCanvas(600, 600)
-    consoleClear()
-    print(mouseX, mouseY)
+    pass
 
 def draw():
-    x = mapping(mouseX(), 0, width(), 0, 255)
-    y = mapping(mouseY(), 0, height(), 0, 255)
-    background(x, y, 255)
-    #print(frameRate())
+    pass
 
 start(setup, draw)
